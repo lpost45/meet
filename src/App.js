7b340cb7b19b56c50b801dd7cd6b59ee9c69967c
lpost45 <lpost45@live.com>
2024-09-02 20:21:36 -0600

    Exercise 4.4 Functionality

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import './App.css';
 import NumberOfEvents from './components/NumberOfEvents';
-
+import mockData from './mock-data';
+import { extractLocations } from './api';
+import { useEffect, useState } from 'react';
 const App = () => {
-  return (
-    <div className="App">
-      <CitySearch />
-      <EventList />
-      <NumberOfEvents />
-    </div>
-  )
+  const [allLocations, setAllLocations] = useState([]);
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [currentCity, setCurrentCity] = useState("See all cities");
+
+  const fetchData = async () => {
+    const allEvents = mockData;
+    const filteredEvents = currentCity === "See all cities" ? allEvents : allEvents.filter(event => event.location === currentCity)
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  }
+  useEffect(() => {
+    fetchData();
+  }, [currentCity, currentNOE]);
+
+return (
+  <div className='App'>
+    <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity}/>
+    <NumberOfEvents setCurrentNOE={setCurrentNOE} />
+    <EventList events={events} />
+  </div>
+)
 }
 
 export default App;

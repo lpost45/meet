7343b5c9af9ca0345bcb4a48e9d89880af29dbd8
lpost45 <lpost45@live.com>
2024-09-18 09:36:22 -0600

    Exercise 4.4 updates

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const CitySearch = ({ allLocations }) => {
+const CitySearch = ({ allLocations, setCurrentCity }) => {
     const [showSuggestions, setShowSuggestions] = useState(false);
     const [query, setQuery] = useState("");
     const [suggestions, setSuggestions] = useState([]);
@@ -8,6 +8,7 @@ const CitySearch = ({ allLocations }) => {
         const value = event.target.textContent;
         setQuery(value);
         setShowSuggestions(false); // to hide the list
+        setCurrentCity(value);
         };
     const handleInputChanged = (event) => {
         const value = event.target.value;

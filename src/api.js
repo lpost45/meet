55cf75ce700682eec651f2785f3e92b59fb1892b
lpost45 <lpost45@live.com>
2024-11-21 10:54:39 -0700

    msg

--- src/api.js
+++ src/api.js
@@ -9,9 +9,9 @@ import mockData from './mock-data';
  * The Set will remove all duplicates from the array.
  */
 export const extractLocations = (events) => {
-  const extractedLocations = events.map((event) => event.location);
-  const locations = [...new Set(extractedLocations)];
-  return locations;
+	const extractedLocations = events.map((event) => event.location);
+	const locations = [...new Set(extractedLocations)];
+	return locations;
 };
 
 /**
@@ -20,73 +20,79 @@ export const extractLocations = (events) => {
  */
 
 const removeQuery = () => {
-  let newurl;
-  if (window.history.pushState && window.location.pathname) {
-    newurl =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname;
-    window.history.pushState("", "", newurl);
-  } else {
-    newurl = window.location.protocol + "//" + window.location.host;
-    window.history.pushState("", "", newurl);
-  }
+	let newurl;
+	if (window.history.pushState && window.location.pathname) {
+		newurl =
+			window.location.protocol +
+			'//' +
+			window.location.host +
+			window.location.pathname;
+		window.history.pushState('', '', newurl);
+	} else {
+		newurl = window.location.protocol + '//' + window.location.host;
+		window.history.pushState('', '', newurl);
+	}
 };
 
 const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const response = await fetch(
-    'https://644sfaq4pf.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
-  );
-  const { access_token } = await response.json();
-  access_token && localStorage.setItem("access_token", access_token);
+	const encodeCode = encodeURIComponent(code);
+	const response = await fetch(
+		'https://644sfaq4pf.execute-api.eu-central-1.amazonaws.com/dev/api/token' +
+			'/' +
+			encodeCode
+	);
+	const { access_token } = await response.json();
+	access_token && localStorage.setItem('access_token', access_token);
 
-  return access_token;
+	return access_token;
 };
 
 const checkToken = async (accessToken) => {
-  const response = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-  );
-  const result = await response.json();
-  return result;
+	const response = await fetch(
+		`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+	);
+	const result = await response.json();
+	return result;
 };
 
 export const getEvents = async () => {
-  if (window.location.href.startsWith('http://localhost')) {
-    return mockData;
-  }
+	if (window.location.href.startsWith('http://localhost')) {
+		return mockData;
+	}
 
-  const token = await getAccessToken();
+	const token = await getAccessToken();
 
-  if (token) {
-    removeQuery();
-    const url = "https://644sfaq4pf.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + "/" + token;
-    const response = await fetch(url);
-    const result = await response.json();
-    if (result) {
-      return result.data.items;
-    } else return null;
-  }
+	if (token) {
+		removeQuery();
+		const url =
+			'https://644sfaq4pf.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' +
+			'/' +
+			token;
+		const response = await fetch(url);
+		const result = await response.json();
+		if (result) {
+			console.log(result);
+			return result;
+		} else return null;
+	}
 };
 
 export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem('access_token');
-  const tokenCheck = accessToken && (await checkToken(accessToken));
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const response = await fetch(
-        "https://644sfaq4pf.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
-      );
-      const result = await response.json();
-      const { authURL } = result;
-      return (window.location.href = authURL);
-    }
-    return code && getToken(code);
-  }
-  return accessToken;
+	const accessToken = localStorage.getItem('access_token');
+	const tokenCheck = accessToken && (await checkToken(accessToken));
+	if (!accessToken || tokenCheck.error) {
+		await localStorage.removeItem('access_token');
+		const searchParams = new URLSearchParams(window.location.search);
+		const code = await searchParams.get('code');
+		if (!code) {
+			const response = await fetch(
+				'https://644sfaq4pf.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url'
+			);
+			const result = await response.json();
+			const { authURL } = result;
+			return (window.location.href = authURL);
+		}
+		return code && getToken(code);
+	}
+	return accessToken;
 };

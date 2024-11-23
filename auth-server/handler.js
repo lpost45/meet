f2a8da13bfd3f2fd7026e9a5a601d6c74c2c6dec
lpost45 <lpost45@live.com>
2024-11-23 08:54:48 -0700

    Exercise 4.7

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { google } = require("googleapis");
+ const { google } = require("googleapis");
 const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;

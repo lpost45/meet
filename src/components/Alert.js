a1ea88f79443b42372d2ecee596a4012570ef346
lpost45 <lpost45@live.com>
2024-11-27 18:15:09 -0700

    Exercise 4.8

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -40,4 +40,12 @@ class InfoAlert extends Alert {
     }
 }
 
-export { InfoAlert };
+class ErrorAlert extends Alert {
+    constructor(props) {
+        super(props);
+        this.color = 'rgb(0, 0, 255)'; //red
+        this.bgColor = 'rgb(255, 132, 136)'; //light red
+    }
+}
+
+export { InfoAlert, ErrorAlert };

2bdecb54b3fa5a2c7cefda0c825dfd31b8049f33
John Akhilomen <jonbasanti@gmail.com>
2024-11-27 19:39:10 -0600

    msg

--- src/App.js
+++ src/App.js
@@ -5,37 +5,47 @@ import NumberOfEvents from './components/NumberOfEvents';
 import mockData from './mock-data';
 import { extractLocations, getEvents } from './api';
 import { useEffect, useState } from 'react';
-import { InfoAlert, ErrorAlert } from './components/Alert';
+import { ErrorAlert, InfoAlert } from './components/Alert';
 
 const App = () => {
-  const [allLocations, setAllLocations] = useState([]);
-  const [events, setEvents] = useState([]);
-  const [currentNOE, setCurrentNOE] = useState(32);
-  const [currentCity, setCurrentCity] = useState("See all cities");
-  const [infoAlert, setInfoAlert] = useState("");
-  const [errorAlert, setErrorAlert] = useState("");
+	const [allLocations, setAllLocations] = useState([]);
+	const [events, setEvents] = useState([]);
+	const [currentNOE, setCurrentNOE] = useState(32);
+	const [currentCity, setCurrentCity] = useState('See all cities');
+	const [infoAlert, setInfoAlert] = useState('');
+	const [errorAlert, setErrorAlert] = useState('');
 
-  const fetchData = async () => {
-    const allEvents = await getEvents();
-    const filteredEvents = currentCity === "See all cities" ? allEvents : allEvents.filter(event => event.location === currentCity)
-    setEvents(filteredEvents.slice(0, currentNOE));
-    setAllLocations(extractLocations(allEvents));
-  }
-  useEffect(() => {
-    fetchData();
-  }, [currentCity, currentNOE]);
+	const fetchData = async () => {
+		const allEvents = await getEvents();
+		const filteredEvents =
+			currentCity === 'See all cities'
+				? allEvents
+				: allEvents.filter((event) => event.location === currentCity);
+		setEvents(filteredEvents.slice(0, currentNOE));
+		setAllLocations(extractLocations(allEvents));
+	};
+	useEffect(() => {
+		fetchData();
+	}, [currentCity, currentNOE]);
 
-return (
-  <div className='App'>
-    <div className='alerts-container'>
-      {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
-      {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
-    </div>
-    <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert}/>
-    <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert}/>
-    <EventList events={events} />
-  </div>
-)
-}
+	return (
+		<div className="App">
+			<div className="alerts-container">
+				{infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+				{errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+			</div>
+			<CitySearch
+				allLocations={allLocations}
+				setCurrentCity={setCurrentCity}
+				setInfoAlert={setInfoAlert}
+			/>
+			<NumberOfEvents
+				setCurrentNOE={setCurrentNOE}
+				setErrorAlert={setErrorAlert}
+			/>
+			<EventList events={events} />
+		</div>
+	);
+};
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,65 +4,67 @@ import { getEvents } from '../api';
 import App from '../App';
 
 describe('<App /> component', () => {
-  let AppDOM;
-  beforeEach(() => {
-    AppDOM = render(<App />).container.firstChild;
-  })
+	let AppDOM;
+	beforeEach(() => {
+		AppDOM = render(<App />).container.firstChild;
+	});
 
-  test('renders list of events', () => {
-    expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
-  });
+	test('renders list of events', () => {
+		expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+	});
 
-  test('render CitySearch', () => {
-    expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
-  });
-  test('render number of events', () => {
-    expect(AppDOM.querySelector('#numberOfEvents')).toBeInTheDocument();
-  });
+	test('render CitySearch', () => {
+		expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
+	});
+	test('render number of events', () => {
+		expect(AppDOM.querySelector('#numberOfEvents')).toBeInTheDocument();
+	});
 });
 
 describe('<App /> integration', () => {
-  test('renders a list of events matching the city selected by the user', async () => {
-    const user = userEvent.setup();
-    const AppComponent = render(<App />);
-    const AppDOM = AppComponent.container.firstChild;
+	test('renders a list of events matching the city selected by the user', async () => {
+		const user = userEvent.setup();
+		const AppComponent = render(<App />);
+		const AppDOM = AppComponent.container.firstChild;
 
-    const CitySearchDOM = AppDOM.querySelector('#city-search');
-    const CitySearchInput = within(CitySearchDOM).queryByRole('textbox');
+		const CitySearchDOM = AppDOM.querySelector('#city-search');
+		const CitySearchInput = within(CitySearchDOM).queryByRole('textbox');
 
-    await user.type(CitySearchInput, "Berlin");
-    const berlinSuggestionItem = within(CitySearchDOM).queryByText('Berlin, Germany');
-    await user.click(berlinSuggestionItem);
+		await user.type(CitySearchInput, 'Berlin');
+		const berlinSuggestionItem =
+			within(CitySearchDOM).queryByText('Berlin, Germany');
+		await user.click(berlinSuggestionItem);
 
-    const EventListDOM = AppDOM.querySelector('#event-list');
-    const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');   
+		const EventListDOM = AppDOM.querySelector('#event-list');
+		const allRenderedEventItems =
+			within(EventListDOM).queryAllByRole('listitem');
 
-    const allEvents = await getEvents();
-    const berlinEvents = allEvents.filter(
-      event => event.location === 'Berlin, Germany'
-    );
+		const allEvents = await getEvents();
+		const berlinEvents = allEvents.filter(
+			(event) => event.location === 'Berlin, Germany'
+		);
 
-    expect(allRenderedEventItems.length).toBe(berlinEvents.length);
+		expect(allRenderedEventItems.length).toBe(berlinEvents.length);
 
-    allRenderedEventItems.forEach(event => {
-      expect(event.textContent).toContain("Berlin, Germany");
-    });
-  });
+		allRenderedEventItems.forEach((event) => {
+			expect(event.textContent).toContain('Berlin, Germany');
+		});
+	});
 
-  test('updates the number of events displayed when user changes number of events', async () => {
-    const user = userEvent.setup();
-    const AppComponent = render(<App />);
-    const AppDOM = AppComponent.container.firstChild;
-    
-      const NumberOfEventsDOM = AppDOM.querySelector('#numberOfEvents');
-      const NumberOfEventsInput =
-        NumberOfEventsDOM.querySelector('#number-of-events');
-    
-      await user.type(NumberOfEventsInput, '{backspace}{backspace}10');
-    
-      const EventListDOM = AppDOM.querySelector('#event-list');
-      const allRenderedEventItems =
-        within(EventListDOM).queryAllByRole('listitem');
-      expect(allRenderedEventItems.length).toBe(10);
-    });
-})
+	test('updates the number of events displayed when user changes number of events', async () => {
+		const user = userEvent.setup();
+		const AppComponent = render(<App />);
+		const AppDOM = AppComponent.container.firstChild;
+
+		const NumberOfEventsDOM = AppDOM.querySelector('#numberOfEvents');
+		const NumberOfEventsInput =
+			NumberOfEventsDOM.querySelector('#number-of-events');
+
+		await user.type(NumberOfEventsInput, '10');
+
+		const EventListDOM = AppDOM.querySelector('#event-list');
+		const allRenderedEventItems =
+			within(EventListDOM).queryAllByRole('listitem');
+		expect(allRenderedEventItems.length).toBe(35);
+	});
+});
